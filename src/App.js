93ff0e908c57cb4c1f1999cbd4d13f6a3449e96a
OlehKobrynovych <o.kobrynovych@gmail.com>
2022-09-23 10:22:15 +0300

    fixsed favoriteProduct

--- src/App.js
+++ src/App.js
@@ -11,33 +11,31 @@ import WishList from './components/WishList/WishList';
 
 function App() {
   return (
-      <div className="App">
-        <div className="app--wrap">
-            <Header />
-            <Routes>
-                <Route path="/" element={<HomeView />} />
-                <Route path="/unisex" element={<ProductFilter />} />
-                <Route path="/unisex/:id" element={<ProductFilter />} />
-                <Route path="/man" element={<ProductFilter />} />
-                <Route path="/man/:id" element={<ProductFilter />} />
-                <Route path="/woman" element={<ProductFilter />} />
-                <Route path="/woman/:id" element={<ProductFilter />} />
-                <Route path="/children" element={<ProductFilter />} />
-                <Route path="/children/:id" element={<ProductFilter />} />
-                <Route path="/collections" element={<ProductFilter />} />
-                <Route path="/collections/:id" element={<ProductFilter />} />
-                <Route path="/discounts" element={<ProductFilter />} />
-                <Route path="/discounts/:id" element={<ProductFilter />} />
+      <div className="app">
+          <Header />
+          <Routes>
+              <Route path="/" element={<HomeView />} />
+              <Route path="/unisex" element={<ProductFilter />} />
+              <Route path="/unisex/:id" element={<ProductFilter />} />
+              <Route path="/man" element={<ProductFilter />} />
+              <Route path="/man/:id" element={<ProductFilter />} />
+              <Route path="/woman" element={<ProductFilter />} />
+              <Route path="/woman/:id" element={<ProductFilter />} />
+              <Route path="/children" element={<ProductFilter />} />
+              <Route path="/children/:id" element={<ProductFilter />} />
+              <Route path="/collections" element={<ProductFilter />} />
+              <Route path="/collections/:id" element={<ProductFilter />} />
+              <Route path="/discounts" element={<ProductFilter />} />
+              <Route path="/discounts/:id" element={<ProductFilter />} />
 
-                <Route path="/wishlist" element={<WishList />} />
-                <Route path="/product/:id" element={<ProductInformation />} />
+              <Route path="/wishlist" element={<WishList />} />
+              <Route path="/product/:id" element={<ProductInformation />} />
 
-                <Route path="/about" element={<AboutUs />} />
-                
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-            <Footer />
-        </div>
+              <Route path="/about" element={<AboutUs />} />
+              
+              <Route path="*" element={<PageNotFound />} />
+          </Routes>
+          <Footer />
       </div>
   );
 }
